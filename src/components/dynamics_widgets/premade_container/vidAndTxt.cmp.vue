f66cff86694abef78298e7422f98c45298c6e88e
Nimrod Gaver <gavernimrod@gmail.com>
2019-11-28 15:51:39 +0200

    nim

--- src/components/dynamics_widgets/premade_container/vidAndTxt.cmp.vue
+++ src/components/dynamics_widgets/premade_container/vidAndTxt.cmp.vue
@@ -1,6 +1,12 @@
 <template>
   <section class="container-twoCol flex row justify-center align-center" :contenteditable="edit">
-      <component v-for="(widget,idx) in data" :key="idx" :is="widget.type" :data="widget.data" :contenteditable="true"></component>
+    <component
+      v-for="(widget,idx) in data"
+      :key="idx"
+      :is="widget.type"
+      :data="widget.data"
+      :contenteditable="true"
+    ></component>
   </section>
 </template>
 
@@ -8,25 +14,36 @@
 import Video from "../video.cmp";
 import Txt from "../txt.cmp";
 
-
 export default {
   components: {
     Video,
     Txt
   },
   props: {
-    edit: Boolean,
+    edit: Boolean
     // widget: Object
   },
-  data(){
-    return{
-      data:[
-                        {"type": "Video", "data":{"url": "https://www.youtube.com/embed/tgbNymZ7vqY"}},{"type": "Txt", "data": {"title": "Imagine", "Txt": "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia!"}}]
-    }
+  data() {
+    return {
+      data: [
+        {
+          type: "Video",
+          data: { url: "https://www.youtube.com/embed/tgbNymZ7vqY" }
+        },
+        {
+          type: "Txt",
+          data: {
+            title: "Imagine",
+            Txt:
+              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto, sed quidem ut dolore iure corrupti expedita. Enim, velit necessitatibus! Mollitia!"
+          }
+        }
+      ]
+    };
   },
-  methods:{
-    updateValue(value){
-      this.$emit('input', value)
+  methods: {
+    updateValue(value) {
+      this.$emit("input", value);
     }
   }
 };
