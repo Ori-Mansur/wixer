ce88b8da9164c7c4d46d728d4a6f3d4f79de0444
Nimrod Gaver <gavernimrod@gmail.com>
2019-12-05 12:58:11 +0200

    responsive wap page

---
+++ src/components/wixer_cmps/FooterApp.vue
@@ -0,0 +1,13 @@
+
+<template>
+ <footer>
+          <h1> wallaYoffiRights &copy; 2019 - Proudly created by Nimrod G. & Meidan L. & Ori M.</h1>
+        </footer> 
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
--- src/components/wixer_cmps/WapList.vue
+++ src/components/wixer_cmps/WapList.vue
@@ -4,7 +4,7 @@
       <!-- <button @click="edit" class="empty-wap">+</button>
                   <li @click="el=!el"  class="li-bar section-add"> -->
                     
-<i class="fa fa-plus empty-wap" @click="edit"></i>
+<i class="fa fa-plus fa-3x empty-wap" @click="edit"></i>
       <!-- <button>P</button> -->
     </li>
     <WapPreview v-for="wap in waps" :key="wap._id" :wap="wap" @select="edit" @preview="preview" />
