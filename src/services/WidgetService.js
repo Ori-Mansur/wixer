089930e463b67039c9799c84a5d435e81c2eaf88
Ori Mansur <orimansur@gmail.com>
2019-12-11 18:24:02 +0200

    dddd

--- src/services/WidgetService.js
+++ src/services/WidgetService.js
@@ -133,120 +133,159 @@ const sections = [
     ]
   },
   {
-    type: 'CardsContainer',
+    _id: 'crd1002',
+    type: 'SectionHorizental',
     name: 'Multi Card',
     icon: 'fa fa-address-card fa-2x',
-    style: {
-      bcgImg: '',
-      bcgColor: ''
-    },
-    data: [
-      {
-        _id: 'crd1001',
-        type: 'Card1',
-        name: 'Card',
-        style: {
-          bcgImg: '',
-          bcgColor: '',
-          height: 90,
-          width: 90
-        },
-        data: [
-          {
-            text: 'Lorem ipsum',
-            style: {
-              color: 'black',
-              fontSize: 22,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
-          },
-          {
-            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            style: {
-              color: 'black',
-              fontSize: 16,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
+    style: { bcgImg: '', bcgColor: '' },
+    data: [{
+      _id: 'card1001',
+      type: 'card',
+      name: 'Card',
+      icon: 'fa fa-align-left fa-2x',
+      sectionList: 'sectionList',
+      style: { bcgImg: '', bcgColor: '' },
+      data: [
+        {
+          _id: 'ca1001',
+          type: 'Img',
+          name: 'Image',
+          icon: 'fa fa-image fa-2x',
+          data: {
+            url: 'https://res.cloudinary.com/dw9jjocrg/image/upload/v1575890067/placeholder-img-4_wty056.jpg'
           }
-        ]
-      },
-      {
-        _id: 'crd1002',
-        type: 'Card1',
-        name: 'Card',
-        style: {
-          bcgImg: '',
-          bcgColor: '',
-          height: 90,
-          width: 90
         },
-        data: [
-          {
-            text: 'Lorem ipsum',
-            style: {
-              color: 'black',
-              fontSize: 22,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
-          },
-          {
-            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            style: {
-              color: 'black',
-              fontSize: 16,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
+        {
+          _id: 'ca1002',
+          type: 'TextEl',
+          name: 'Title',
+          icon: 'fa fa-font fa-2x',
+          text: 'Insert Title',
+          style: {
+            color: 'black',
+            fontSize: 30,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
           }
-        ]
-      },
-      {
-        _id: 'crd1003',
-        type: 'Card1',
-        name: 'Card',
-        style: {
-          bcgImg: '',
-          bcgColor: '',
-          height: 90,
-          width: 90
         },
-        data: [
-          {
-            text: 'Lorem ipsum',
-            style: {
-              color: 'black',
-              fontSize: 22,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
-          },
-          {
-            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            style: {
-              color: 'black',
-              fontSize: 16,
-              fontWeight: 'normal',
-              fontFamily: 'Arial',
-              fontStyle: 'normal',
-              txtAlign: 'center'
-            }
+        {
+          _id: 'ca1003',
+          type: 'Txt',
+          name: 'Text',
+          icon: 'fa fa-font fa-3x',
+          text:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto.',
+          style: {
+            color: 'black',
+            fontSize: 16,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
           }
-        ]
-      }
-    ]
+        },
+      ]
+    }, {
+      _id: 'card1002',
+      type: 'card',
+      name: 'Card',
+      icon: 'fa fa-align-left fa-2x',
+      sectionList: 'sectionList',
+      style: { bcgImg: '', bcgColor: '' },
+      data: [
+        {
+          _id: 'ca1001',
+          type: 'Img',
+          name: 'Image',
+          icon: 'fa fa-image fa-2x',
+          data: {
+            url: 'https://res.cloudinary.com/dw9jjocrg/image/upload/v1575890067/placeholder-img-4_wty056.jpg'
+          }
+        },
+        {
+          _id: 'ca1002',
+          type: 'TextEl',
+          name: 'Title',
+          icon: 'fa fa-font fa-2x',
+          text: 'Insert Title',
+          style: {
+            color: 'black',
+            fontSize: 30,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
+          }
+        },
+        {
+          _id: 'ca1003',
+          type: 'Txt',
+          name: 'Text',
+          icon: 'fa fa-font fa-3x',
+          text:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto.',
+          style: {
+            color: 'black',
+            fontSize: 16,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
+          }
+        },
+      ]
+    }, {
+      _id: 'card1003',
+      type: 'card',
+      name: 'Card',
+      icon: 'fa fa-align-left fa-2x',
+      sectionList: 'sectionList',
+      style: { bcgImg: '', bcgColor: '' },
+      data: [
+        {
+          _id: 'ca1001',
+          type: 'Img',
+          name: 'Image',
+          icon: 'fa fa-image fa-2x',
+          data: {
+            url: 'https://res.cloudinary.com/dw9jjocrg/image/upload/v1575890067/placeholder-img-4_wty056.jpg'
+          }
+        },
+        {
+          _id: 'ca1002',
+          type: 'TextEl',
+          name: 'Title',
+          icon: 'fa fa-font fa-2x',
+          text: 'Insert Title',
+          style: {
+            color: 'black',
+            fontSize: 30,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
+          }
+        },
+        {
+          _id: 'ca1003',
+          type: 'Txt',
+          name: 'Text',
+          icon: 'fa fa-font fa-3x',
+          text:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab laboriosam aliquam, porro harum libero assumenda modi illum placeat iusto.',
+          style: {
+            color: 'black',
+            fontSize: 16,
+            fontWeight: 'normal',
+            fontFamily: 'Arial',
+            fontStyle: 'normal',
+            txtAlign: 'center'
+          }
+        },
+      ]
+    },]
   },
   {
     type: 'NavBar',
@@ -295,7 +334,7 @@ const sections = [
     style: { bcgImg: '', bcgColor: '' },
     data: []
   },
- 
+
   {
     _id: '0001',
     type: 'Map',
@@ -306,7 +345,7 @@ const sections = [
       currentPlace: null,
       locations: [],
       center: { lat: 32.109333, lng: 34.855499 },
-      businessInfo: {openOn:{week:'', sat:'', sun:''}, email:'', phone:''}
+      businessInfo: { openOn: { week: '', sat: '', sun: '' }, email: '', phone: '' }
 
     }
   },
@@ -338,7 +377,7 @@ const elements = [
       txtAlign: 'center'
     }
   },
-  
+
   {
     _id: 'el1002',
     type: 'Video',
@@ -422,7 +461,7 @@ const elements = [
       },
     ]
   },
-  
+
   // {
   //   _id: 'el1004',
   //   type: 'FormV',
