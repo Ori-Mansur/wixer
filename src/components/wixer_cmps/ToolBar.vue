cea3d01678f42e078017c0e23a2697a487e737bf
MeidanLeidemran <meidan.leiderman@gmail.com>
2019-12-15 13:28:29 +0200

    updated toolbar functionality

--- src/components/wixer_cmps/ToolBar.vue
+++ src/components/wixer_cmps/ToolBar.vue
@@ -5,31 +5,35 @@
         <Menu />
       </div>
       <div class="list-group-container">
-        <li @click="section=!section" class="li-bar section-add">
+        <li @click="activateSection" class="li-bar section-add">
           <unicon name="book-medical" :fill="color" />
-          <h4 :style="{color:color}">Sections</h4>
+          <h4 :style="{ color: color }">Sections</h4>
         </li>
-        <li @click="el=!el" class="li-bar section-add">
+        <li @click="activateElement" class="li-bar section-add">
           <unicon name="book-medical" :fill="elColor" />
-          <h4 :style="{color:elColor}">Elements</h4>
+          <h4 :style="{ color: elColor }">Elements</h4>
         </li>
       </div>
       <div class="list-group-content">
-        <div v-if="section">
+        <div v-if="isSection">
           <draggable
             class="dragArea list-group"
             :list="widgets"
             :sort="false"
             :group="{ name: 'people', pull: 'clone', put: false }"
           >
-            <div class="list-group-item" v-for="(element,idx) in widgets" :key="idx">
+            <div
+              class="list-group-item"
+              v-for="(element, idx) in widgets"
+              :key="idx"
+            >
               <i :class="element.icon" />
               {{ element.name }}
             </div>
           </draggable>
         </div>
 
-        <div v-if="el">
+        <div v-if="isElement">
           <draggable
             class="dragArea list-group"
             :list="elements"
@@ -37,7 +41,11 @@
             :group="{ name: 'element', pull: 'clone', put: false }"
             :clone="cloneX"
           >
-            <div class="list-group-item" v-for="element in elements" :key="element._id">
+            <div
+              class="list-group-item"
+              v-for="element in elements"
+              :key="element._id"
+            >
               <i :class="element.icon" />
               {{ element.name }}
             </div>
@@ -45,61 +53,37 @@
         </div>
       </div>
 
-      <li @click="edit=!edit" class="li-bar section-edit">
+      <li @click="activateSettings" class="li-bar section-edit">
         <i class="fa fa-cog"></i>
-        <h3 :style="{color:colorSections}">Settings</h3>
+        <h3 :style="{ color: colorSections }">Settings</h3>
       </li>
-      <div v-if="edit" class="list-option wap-option">
+      <div v-if="isSettings" class="list-option wap-option">
         <SettingWap @setName="add" :nav="nav" />
       </div>
 
       <div class="btn-container">
         <li class="flex" @click="$emit('preview')">
           <i class="fa fa-eye fa-2x"></i>
-          <h4 :style="{color:'gray'}"></h4>
+          <h4 :style="{ color: 'gray' }"></h4>
         </li>
         <li class="flex" @click="share">
           <i class="fa fa-share-alt-square fa-2x"></i>
-          <h4 :style="{color:'gray'}"></h4>
+          <h4 :style="{ color: 'gray' }"></h4>
         </li>
         <li class="flex" @click="save">
           <i class="fa fa-save fa-2x"></i>
 
-          <h4 :style="{color:'white'}"></h4>
+          <h4 :style="{ color: 'white' }"></h4>
         </li>
         <router-link to="/wap">
           <li class="flex">
             <i class="fa fa-sign-out fa-2x"></i>
 
-            <h4 :style="{color:'gray'}"></h4>
+            <h4 :style="{ color: 'gray' }"></h4>
           </li>
         </router-link>
       </div>
-<ShareModal :isShare="isShare" @close="isShare=false"/>
-      <!-- <div class="section-publish-container">
-        <li @click="$emit('preview')" class="li-bar section-publish">
-          <i class="fa fa-eye"></i>
-          <h4 :style="{color:'gray'}">Preview</h4>
-        </li>
-        <li @click="save" class="li-bar section-publish">
-          <i class="fa fa-share-alt-square"></i>
-          <h4 :style="{color:'gray'}">Share</h4>
-        </li>
-      </div>
-      <div class="section-save-container">
-        <li @click="save" class="li-bar section-save">
-          <i class="fa fa-save"></i>
-
-          <h4 :style="{color:'white'}">Save</h4>
-        </li>
-        <router-link to="/wap">
-          <li class="li-bar section-save">
-            <i class="fa fa-sign-out"></i>
-
-            <h4 :style="{color:'gray'}">Cancel</h4>
-          </li>
-        </router-link>
-      </div>-->
+      <ShareModal :isShare="isShare" @close="isShare = false" />
     </ul>
   </div>
 </template>
@@ -107,7 +91,7 @@
 import SettingWap from "./SettingWap.vue";
 import draggable from "vuedraggable";
 import Menu from "./Menu.vue";
-import ShareModal from './ShareModal.vue'
+import ShareModal from "./ShareModal.vue";
 
 export default {
   props: {
@@ -125,10 +109,28 @@ export default {
       menu: false,
       gray: "red",
       isPreview: true,
-      isShare:false
+      isShare: false,
+      isSection: false,
+      isElement: false,
+      isSettings: false
     };
   },
   methods: {
+    activateElement(){
+      this.isSetting=false
+      this.isSection=false
+      this.isElement===true? this.isElement=false: this.isElement=true
+    },
+    activateSection(){
+      this.isSetting = false
+      this.isElement=false
+      this.isSection===true? this.isSection=false: this.isSection=true
+    },
+    activateSettings(){
+      this.isElement=false
+      this.isSection=false
+      this.isSettings===true? this.isSettings=false: this.isSettings=true
+    },
     add(name) {
       this.edit = false;
       this.$store.commit({ type: "setName", name });
@@ -139,9 +141,9 @@ export default {
     save() {
       this.$emit("save");
     },
-    share(){
-// this.save()
-this.isShare=true
+    share() {
+      // this.save()
+      this.isShare = true;
     },
     cloneX(section) {
       console.log(section);
@@ -153,6 +155,10 @@ this.isShare=true
     }
   },
   computed: {
+    isActive() {
+      if (this.activeList === "section") return true;
+      else return false;
+    },
     color() {
       if (this.section) return "#6ACEF9";
       else return "gray";
@@ -190,5 +196,3 @@ this.isShare=true
   }
 };
 </script>
-
-
